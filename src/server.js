d3ebbbc89e62ef2b24945a743cd2b0d23c478993
thanhthu1410 <thanhthu9c@gmail.com>
2023-07-25 13:59:21 +0700

    updateSQLConfig

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ dotenv.config();
 // tao server
 import express from  'express';
 const server = express();
-
+import {mysqlConnect} from './databases/mySQL'
+mysqlConnect()
 server.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on: ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/`);
 })
